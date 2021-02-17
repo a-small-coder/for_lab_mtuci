1f91abd021dae6f171f8d92b43566ecbb8804241
oleg <dubrovin.oleg.v@mail.ru>
2021-02-17 20:21:02 +0300

    lab3 completed

---
+++ lab3/Location.java
@@ -0,0 +1,48 @@
+/**
+ * This class represents a specific location in a 2D map.  Coordinates are
+ * integer values.
+ **/
+public class Location
+{
+    /** X coordinate of this location. **/
+    public int xCoord;
+
+    /** Y coordinate of this location. **/
+    public int yCoord;
+
+
+    /** Creates a new location with the specified integer coordinates. **/
+    public Location(int x, int y)
+    {
+        xCoord = x;
+        yCoord = y;
+    }
+
+    /** Creates a new location with coordinates (0, 0). **/
+    public Location()
+    {
+        this(0, 0);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        Location guest = (Location) obj;
+        return xCoord == guest.xCoord && yCoord == guest.yCoord;
+    }
+
+    @Override
+    public int hashCode(){
+        final int prime = 37;
+        int result= 15;
+
+        result = prime * result + (int)xCoord;
+        result = prime * result + (int)yCoord;
+        return result;
+    }
+}
---
+++ learning_java/Main.java
@@ -0,0 +1,38 @@
+package java_mtuci.learning_java;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        showTypes();
+        readConsole();
+    }
+    public static String readConsole(){
+        Scanner str = new Scanner(System.in);
+        String first_str = str.nextLine();
+        System.out.println("You input: " + first_str);
+        str.close();
+        return first_str;
+    }
+    public static void showTypes(){
+        System.out.println("Byte - it's a similar number type");
+        byte num_in_byte = 15;
+        System.out.println(num_in_byte);
+        System.out.println("Int - it's a most popular number type");
+        int num_in_integer = 15000;
+        System.out.println(num_in_integer);
+        System.out.println("Long - it's a large number type");
+        long num_in_long = 15000000;
+        System.out.println(num_in_long);
+        System.out.println("Double - it's a number with dot type");
+        double num_in_double = 15000000.123123;
+        System.out.println(num_in_double);
+        System.out.println("Char - it's a symbol type");
+        char sym = 'A';
+        System.out.println(sym);
+        System.out.println("Bollean - it's a logic type");
+        boolean is_true = true;
+        System.out.println(is_true);
+
+    }
+}
